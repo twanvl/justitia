f3789abd5f429aa6b81abeb7dc46af5193bd4f7e
twan <twan@516817d7-8a10-9e40-5960-5d66bc2d3a14>
2009-08-25 13:09:40 +0000

    redirect, etc

---
+++ frontend/lib/Util.php
@@ -0,0 +1,29 @@
+<?php
+
+// -----------------------------------------------------------------------------
+// Utility functions
+// -----------------------------------------------------------------------------
+
+class Util {
+	// Redirects the user to $url.
+	static function redirect($url) {
+		if ($url == '') {
+			$url = 'index.php';
+		}
+		header("HTTP/1.1 301 Moved Permanently");
+		header("Location: $url");
+		echo "This resource may be found at <a href=\"$url\">$url</a>.";
+		exit();
+	}
+	
+	static function current_url() {
+		//$_SERVER["PATH_INFO"]
+		// TODO: strip base url?
+		return $_SERVER['REQUEST_URI'];
+	}
+	
+	// Redirects the user to the login page
+	static function login() {
+		Util::redirect("login.php?redirect=" . urlencode(Util::current_url()));
+	}
+}
