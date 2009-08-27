2a329d1f9dac500be0bb7dcf351eefe3f442dbb9
twan <twan@516817d7-8a10-9e40-5960-5d66bc2d3a14>
2009-08-27 17:56:26 +0000

    hoe security te krijgen

--- config/config.php
+++ config/config.php
@@ -1,17 +1,20 @@
 <?php
 
-// where are courses located?
+// -----------------------------------------------------------------------------
+// Directories
+// -----------------------------------------------------------------------------
+
+// Course data files
 define('COURSE_DIR',       '../courses');
-define('SUBMISSION_DIR',   '../submissions');
+// Incomming submissions
 define('PENDING_DIR',      '../submissions/pending');
+// Judged submissions
+define('SUBMISSION_DIR',   '../submissions');
 define('TEMP_JUDGING_DIR', ''); // empty string for system wide temp dir
 
-// what time are we in?
-define('TIMEZONE', 'Europe/Paris');
-
-// how long before submissions are re-judged if no answer has yet been received (in seconds)?
-//define('REJUDGE_TIMEOUT', 120);
-define('REJUDGE_TIMEOUT', 1);//Debug
+// -----------------------------------------------------------------------------
+// Database
+// -----------------------------------------------------------------------------
 
 // the database connection
 define('DB_PATH',     'mysql:host=localhost;dbname=new_athena');
@@ -19,20 +22,34 @@ define('DB_USERNAME', 'new_athena');
 define('DB_PASSWORD', 'Klqonhtak7');
 define('DB_PERSISTENT', true);
 
+// -----------------------------------------------------------------------------
+// Other options
+// -----------------------------------------------------------------------------
+
+// what timezone are we in?
+define('TIMEZONE', 'Europe/Paris');
+
+// how long before submissions are re-judged if no answer has yet been received (in seconds)?
+//define('REJUDGE_TIMEOUT', 120);
+define('REJUDGE_TIMEOUT', 1);//Debug
+
+// -----------------------------------------------------------------------------
 // default attribute values
-$attribute_defaults['allow archives'] = false;
-$attribute_defaults['submitable']     = false;
-$attribute_defaults['compile']        = true;
-$attribute_defaults['filename regex'] = '';
-$attribute_defaults['visible']        = true;
-$attribute_defaults['show date']      = 'always';
-$attribute_defaults['hide date']      = 'never';
-$attribute_defaults['start date']     = 'always';
-$attribute_defaults['end date']       = 'never';
-$attribute_defaults['time limit']     = 1;
-$attribute_defaults['memory limit']   = 100000;
-$attribute_defaults['keep best']      = true;
-$attribute_defaults['show compile errors'] = true;
-$attribute_defaults['show run errors']     = 'all';
-$attribute_defaults['show input/output']   = 'none';
-$attribute_defaults['max group size'] = 1e100;
+// -----------------------------------------------------------------------------
+
+$attribute_defaults['allow archives']		= false;
+$attribute_defaults['submitable']		= false;
+$attribute_defaults['compile']			= true;
+$attribute_defaults['filename regex']		= '';
+$attribute_defaults['visible']			= true;
+$attribute_defaults['show date']		= 'always';
+$attribute_defaults['hide date']		= 'never';
+$attribute_defaults['start date']		= 'always';
+$attribute_defaults['end date']			= 'never';
+$attribute_defaults['max group size']		= 1e100;
+$attribute_defaults['keep best']		= true;
+$attribute_defaults['time limit']		= 1;
+$attribute_defaults['memory limit']		= 100000;
+$attribute_defaults['show compile errors']	= true;
+$attribute_defaults['show run errors']		= 'all';
+$attribute_defaults['show input/output']	= 'none';
