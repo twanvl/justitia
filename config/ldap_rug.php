8f01e1f806bbba16dd58f27e79ad98724399a6bc
twan <twan@516817d7-8a10-9e40-5960-5d66bc2d3a14>
2010-01-22 14:24:50 +0000

    ldap blaat

--- config/ldap_rug.php
+++ config/ldap_rug.php
@@ -27,17 +27,32 @@ function ldap_connect_and_login($login,$password) {
 	return $con;
 }
 
+function starts_upper($x) {
+	return !empty($x) && strtoupper($x[0]) === $x[0];
+}
+
+// Split a name like "T.M. van Laarhoven" into ("T.M.","van","Laarhoven")
 function split_name($fullname) {
 	$words = explode(' ',$fullname);
-	// TODO
-	//$firstname = $words[0];
-	//return array($firstname,$midname,$lastname);
-	return array('','',$fullname);
+	if (count($words) < 2) return array('','',$fullname);
+	$first = array_shift($words);
+	// Tussenvoegsels beginnen met een kleine letter
+	$mid = array();
+	while (!empty($words) && !starts_upper($words[0])) {
+	    $mid []= array_shift($words);
+	}
+	// er is altijd een achternaam
+	if (empty($words)) {
+	   $words = $mid; $mid = array();
+	}
+	$name = array($first,implode(' ',$mid),implode(' ',$words));
+	return $name;
 }
 
 function userdata_from_ldap($entry) {
-	$name = $entry['lwpfullname'];
-	$email = $entry['lwpmailaddress'];
+	if (!isset($entry['lwpfullname'],$entry['lwpmailaddress'])) return false;
+	$name = $entry['lwpfullname'][0];
+	$email = $entry['lwpmailaddress'][0];
 	$class = strpos($entry['dn'],'student')!==false ? 'student' : 'staff';
 	list($firstname,$midname,$lastname) = split_name($name);
 	return array(
