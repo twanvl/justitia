7408d20b09389868db9237c435e556cde0dd57af
twanvl <twanvl@516817d7-8a10-9e40-5960-5d66bc2d3a14>
2010-05-02 13:29:32 +0000

    when there are multiple files, allow downloading them all as a zip file

--- frontend/download_entity.php
+++ frontend/download_entity.php
@@ -10,12 +10,27 @@ $user = Authentication::require_user();
 $entity = Entity::get(@$_SERVER['PATH_INFO'], !$user->is_admin);
 
 // Which file are we downloading?
-$filename = @$_REQUEST['f'];
-$files = $entity->downloadable_files();
-if (!in_array($filename,$files)) {
-	die("You have no rights to view this file.");
+if (isset($_REQUEST['all'])) {
+	// download all files as a zip archive	
+	$filepath = tempnam('','zip');
+	$delete_temp_file = $filepath;
+	$zip = new ZipArchive();
+	$zip->open($filepath,ZIPARCHIVE::CREATE);
+	$files = $entity->downloadable_files();
+	foreach($files as $filename) {
+		$zip->addFile($entity->data_path() . $filename, $filename);
+	}
+	$zip->close();
+	$filename = $entity->dir_name() . '.zip';
+	$filetype = 'application/zip';
+} else {
+	$filename = @$_REQUEST['f'];
+	$files = $entity->downloadable_files();
+	if (!in_array($filename,$files)) {
+		die("You have no rights to view this file.");
+	}
+	$filepath = $entity->data_path() . $filename;
 }
-$filepath = $entity->data_path() . $filename;
 
 // Does the file exist?
 if (!file_exists($filepath)) {
@@ -23,8 +38,14 @@ if (!file_exists($filepath)) {
 }
 
 header("Content-Disposition: attachment; filename=$filename");
-header("Content-Type: " . Util::content_type($filepath));
+header("Content-Type: " . (isset($filetype) ? $filetype : Util::content_type($filepath)));
 
 // Open file and pass it through
 $fp = fopen($filepath, "rb");
 fpassthru($fp);
+fclose($fp);
+
+// Cleanup
+if (isset($delete_temp_file)) {
+	@unlink($delete_temp_file);
+}
